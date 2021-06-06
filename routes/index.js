ca67ca7fea1a9cdf0254001d5160fd5de987b950
filiptarajko <filiptarajko@gmail.com>
2021-06-07 00:56:27 +0200

    add date to timestamps, delete predefined messages

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-const messages = [
-  {
-    text: "Hi there!",
-    user: "Peter",
-    added: new Date()
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date()
-  }
-];
+const messages = [];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
